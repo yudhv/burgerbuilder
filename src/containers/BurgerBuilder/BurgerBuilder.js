e23834f1c09716bb8da316e3298396569abc1382
yudhv <yudhvirx@gmail.com>
2019-02-25 19:46:26 -0500

    Add Layout and BurgerIngredients components. Also add CSS for the latter

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+
+class BurgerBuilder extends Component {
+    render() {
+        return(<Aux>
+            <div>Burger</div>
+            <div>Build Tools</div>
+        </Aux>)
+    }
+}
+
+export default BurgerBuilder;
