4d26531c8497962a49bd231667cd28349620dff1
yudhv <yudhvirx@gmail.com>
2019-02-27 11:45:30 -0500

    Updates

---
+++ precache-manifest.5a55517d1d53ee9234e21bbb03d0153c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "54bf60a7f6cb7bb60cab",
+    "url": "/burgerbuilder/static/js/runtime~main.54bf60a7.js"
+  },
+  {
+    "revision": "6f367885ff980079be72",
+    "url": "/burgerbuilder/static/js/main.6f367885.chunk.js"
+  },
+  {
+    "revision": "07af7f008eef03db5381",
+    "url": "/burgerbuilder/static/js/2.07af7f00.chunk.js"
+  },
+  {
+    "revision": "6f367885ff980079be72",
+    "url": "/burgerbuilder/static/css/main.ca07ec39.chunk.css"
+  },
+  {
+    "revision": "cec43ec99d2dd0bb8b411e9b2467119c",
+    "url": "/burgerbuilder/index.html"
+  }
+];
